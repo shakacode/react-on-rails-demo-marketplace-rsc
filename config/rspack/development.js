d9c1e3662b5182933dfb346ed28ab0f4a24f5fe0
Justin Gordon <justin@shakacode.com>
2026-07-04 09:41:55 -1000

    Support Rspack React Refresh v2 export

--- config/rspack/development.js
+++ config/rspack/development.js
@@ -7,10 +7,10 @@ const rspackConfig = require('./ServerClientOrBoth');
 const developmentEnvOnly = (clientConfig, _serverConfig) => {
   if (inliningCss) {
     try {
-      const ReactRefreshPlugin = require('@rspack/plugin-react-refresh');
-      clientConfig.plugins.push(
-        new ReactRefreshPlugin(),
-      );
+      const reactRefreshPlugin = require('@rspack/plugin-react-refresh');
+      const ReactRefreshPlugin =
+        reactRefreshPlugin.ReactRefreshRspackPlugin || reactRefreshPlugin.default || reactRefreshPlugin;
+      clientConfig.plugins.push(new ReactRefreshPlugin());
     } catch {
       // @rspack/plugin-react-refresh not installed, skip
     }
