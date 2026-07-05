fa629ca8fa2f54772fa362d2049e2e8e351cac10
Justin Gordon <justin@shakacode.com>
2026-07-04 21:51:26 -1000

    Add Rspack dev-server dependency

--- config/rspack/development.js
+++ config/rspack/development.js
@@ -8,6 +8,7 @@ const developmentEnvOnly = (clientConfig, _serverConfig) => {
   if (inliningCss) {
     try {
       const reactRefreshPlugin = require('@rspack/plugin-react-refresh');
+      // v1 exported the constructor directly; v2 returns a namespace with this named export.
       const ReactRefreshPlugin =
         reactRefreshPlugin.ReactRefreshRspackPlugin || reactRefreshPlugin.default || reactRefreshPlugin;
       clientConfig.plugins.push(new ReactRefreshPlugin());
