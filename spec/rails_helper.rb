e6af61279b5153fdd6c49b66ededa64d8a4b662f
Justin Gordon <justin@shakacode.com>
2026-07-18 02:32:00 -1000

    Prepare test schema for direct RSpec runs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,12 @@ require 'rspec/rails'
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+
 Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |file| require file }
 
 RSpec.configure do |config|
